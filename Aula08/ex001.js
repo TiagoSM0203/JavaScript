8e35eb611496295bc3eb5cb9eedda60711fd7616
TiagoSM0203 <109996403+TiagoSM0203@users.noreply.github.com>
2024-09-24 20:39:26 -0300

    desafio de JS

---
+++ Aula08/ex001.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log('a velocidade do seu carro e ' + vel + 'Km/h')
+if (vel > 60) {
+    console.log(`vc ultrapassou a velocidade permitida`) 
+} else {
+    console.log('parabens otario!')    
+}
+console.log(`Dirija sempre usando cinto de seguranca`)
---
+++ Aula08/ex002.js
@@ -0,0 +1,6 @@
+var pais = 'Franca'
+if (pais == 'Brasil') {
+    console.log('brasileiro')
+} else {
+    console.log('vc e gringo')
+}
