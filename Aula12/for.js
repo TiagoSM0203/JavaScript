9dd3ec3d61a78f0f9aa8e2d881cadd59d8413e19
TiagoSM0203 <109996403+TiagoSM0203@users.noreply.github.com>
2024-09-26 06:25:03 -0300

    for

---
+++ Aula12/for.js
@@ -0,0 +1,13 @@
+console.log('vai comecar...')
+for(var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+console.log('FIM')
+
+/*
+var c = 1
+while (c <= 20) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+*/
