1b4b2c7fdbd4a23be1d7cd5d457a0b454178894d
TiagoSM0203 <109996403+TiagoSM0203@users.noreply.github.com>
2024-09-26 06:55:51 -0300

    ex014

---
+++ Desafio/ex013/script.js
@@ -0,0 +1,11 @@
+function contar() {
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Digite um numero para comecar!') 
+    } else {
+        
+    }
+}
