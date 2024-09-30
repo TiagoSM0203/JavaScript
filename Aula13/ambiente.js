0ae120660b3e222b2d5a437cbc22b7095c38f7f9
TiagoSM0203 <109996403+TiagoSM0203@users.noreply.github.com>
2024-09-30 20:22:56 -0300

    array

---
+++ Aula13/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8 ,3 , 9, 2]
+num.push(4)
+
+console.log(`o vetor tem ${num.length} posicoes`)
+console.log(`o vetor tem ${num[4]} posicoes`)
+let pos = num.indexOf(4)
+console.log(`O valor 4 esta na posicao ${pos}`)
